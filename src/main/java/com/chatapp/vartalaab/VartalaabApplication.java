3b95db8a0d3e6566acd49bd8d5efde55ffb0e6fd
RJ2523 <rajneesh.sahay79@gmail.com>
2024-02-08 09:42:09 +0530

    first commit

---
+++ src/main/java/com/chatapp/vartalaab/VartalaabApplication.java
@@ -0,0 +1,13 @@
+package com.chatapp.vartalaab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VartalaabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VartalaabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chatapp/vartalaab/VartalaabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatapp.vartalaab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VartalaabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
