2e0d236e2ebabec8c0129280f91ad4bf65a10c4b
pippa-singh <mayukha2607@gmail.com>
2024-07-29 00:41:21 +0530

    Restricts fetching chats that don't belong to the authorized user

--- src/main/java/com/chatapp/vartalaab/controller/LoginController.java
+++ src/main/java/com/chatapp/vartalaab/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ import com.chatapp.vartalaab.utils.GeneralUtility;
 
 import lombok.extern.slf4j.Slf4j;
 
-
+@CrossOrigin
 @RestController
 @Slf4j
 public class LoginController{
--- src/main/java/com/chatapp/vartalaab/repository/MongoMessageRepository.java
+++ src/main/java/com/chatapp/vartalaab/repository/MongoMessageRepository.java
@@ -5,10 +5,12 @@ import com.chatapp.vartalaab.document.Message;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.List;
+
 public interface MongoMessageRepository extends MongoRepository<Message, String> {
 
     @Query("{id:{ $regex: '?0' } }")
-    Message findByChatId(String chatId);
+    List<Message> findByChatId(String chatId);
 
     boolean existsById(String chatId);
 }
